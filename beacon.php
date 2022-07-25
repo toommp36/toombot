51d52407098416ebfa3306ad76f003aa728ded2c
toommp36 <109773775+toommp36@users.noreply.github.com>
2022-07-25 10:59:30 +0700

    Update beacon.php

--- beacon.php
+++ beacon.php
@@ -166,8 +166,8 @@ function send_reply_message($url, $post_header, $post_body)
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    //curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-    //curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
     $result = curl_exec($ch);
     curl_close($ch);
