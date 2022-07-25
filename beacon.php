b3cdb851c5d85d4f0fe62c6d78979e0e4c79926a
toommp36 <109773775+toommp36@users.noreply.github.com>
2022-07-25 11:24:47 +0700

    Update beacon.php

--- beacon.php
+++ beacon.php
@@ -123,7 +123,7 @@ $jsonFlex = [
             "action" => [
               "type" => "uri",
               "label" => "View Details",
-              "url" => "https:/youtube.com/"
+              "url" => "https://youtube.com/"
             ]
           ]
         ]
